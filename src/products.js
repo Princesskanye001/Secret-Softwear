3f53c2d1c746d94e4cba9ae06212af0e49d6ff3f
Princess <wtc@pop-os.localdomain>
2024-11-07 10:22:37 +0200

    my clothing brand

---
+++ src/products.js
@@ -0,0 +1,59 @@
+import product1 from "./assets/products/blackhoodie-removebg-preview.png";
+import product2 from "./assets/products/whitehoodie-removebg-preview.png";
+import product3 from "./assets/products/image_123650291-removebg-preview(1).png";
+import product4 from "./assets/products/blackbeanie.JPG";
+import product5 from "./assets/products/greybeanie.JPG";
+import product6 from "./assets/products/blackbuckethat.JPG";
+import product7 from "./assets/products/khakibuckethat.JPG";
+import product8 from "./assets/products/cap-removebg-preview.png";
+
+export const PRODUCTS = [
+  {
+    id: 1,
+    productName: "Black Hoodie",
+    price: 350.0,
+    productImage: product1,
+  },
+  {
+    id: 2,
+    productName: "White Hoodie",
+    price: 350.0,
+    productImage: product2,
+  },
+  {
+    id: 3,
+    productName: "T-shirt",
+    price: 300.0,
+    productImage: product3,
+  },
+  {
+    id: 4,
+    productName: "Black Beanie",
+    price: 80.0,
+    productImage: product4,
+  },
+  {
+    id: 5,
+    productName: "Grey Beanie",
+    price: 80.0,
+    productImage: product5,
+  },
+  {
+    id: 6,
+    productName: "Black Bucket Hat",
+    price: 100.0,
+    productImage: product6,
+  },
+  {
+    id: 7,
+    productName: "Khaki Bucket Hat",
+    price: 100.0,
+    productImage: product7,
+  },
+  {
+    id: 8,
+    productName: "Cap",
+    price: 100.0,
+    productImage: product8,
+  },
+];
